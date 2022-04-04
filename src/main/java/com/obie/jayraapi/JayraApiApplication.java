cfa999d1f557258326f95706ad87cf814d9c1a80
missjhai <iamjeajae@gmail.com>
2022-04-04 21:44:45 +0800

    First release

---
+++ src/main/java/com/obie/jayraapi/JayraApiApplication.java
@@ -0,0 +1,13 @@
+package com.obie.jayraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayraApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JayraApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/obie/jayraapi/JayraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obie.jayraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JayraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
