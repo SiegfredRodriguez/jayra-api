d5a9d4567382ca9209b38688e216c2d1b23d9995
missjhai <iamjeajae@gmail.com>
2022-04-06 14:51:35 +0800

    001: Controller Ticket updates DELETE/UPDATE

--- src/main/java/com/obie/jayraapi/controller/TicketController.java
+++ src/main/java/com/obie/jayraapi/controller/TicketController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ticket")
@@ -36,7 +38,7 @@ public class TicketController {
         ticketlist.add(ticket);
     }
 
-    @PostMapping("/updateTicket/{ticketNum}/{title}")
+    @PatchMapping("/{ticketNum}/{title}")
     public void updateTicket(@PathVariable int ticketNum, @PathVariable String title) {
         if ((ticketlist == null) || ticketlist.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
@@ -49,18 +51,24 @@ public class TicketController {
         }
     }
 
-    @PostMapping("/deleteTicket/{ticketNum}")
-    public void deleteTicket(@PathVariable int ticketNum) {
+    @DeleteMapping("/{ticketNum}")
+    public Map<String, Boolean> deleteTicket(@PathVariable(value = "ticketNum") int ticketNum) {
         if ((ticketlist == null) || ticketlist.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+        Map<String, Boolean> response = new HashMap<>();
 
         for (int i = 0; i < ticketlist.size(); i++) {
             Ticket ticket = ticketlist.get(i);
             if (ticket.getTicketNum() == ticketNum) {
                 ticketlist.remove(i);
+                response.put("deleted", Boolean.TRUE);
             }
         }
+        if (response.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return response;
     }
 
     @GetMapping("/getAllTickets")
