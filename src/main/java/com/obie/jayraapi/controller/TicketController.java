5a71b603db39330ca6cfb47b94d75b6b00c06a9e
missjhai <iamjeajae@gmail.com>
2022-04-06 12:17:12 +0800

    001: Controller Ticket updates CRUD

--- src/main/java/com/obie/jayraapi/controller/TicketController.java
+++ src/main/java/com/obie/jayraapi/controller/TicketController.java
@@ -4,26 +4,80 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ticket")
 public class TicketController {
 
-    private Ticket ticket;
+    private List<Ticket> ticketlist = new ArrayList<>();
 
     @GetMapping("/{ticketNum}")
     public Ticket getTicket(@PathVariable int ticketNum) {
-        if (ticket == null) {
+        if ((ticketlist == null) || ticketlist.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        if (ticket.getTicketNum() == ticketNum) {
-            return ticket;
-        } else {
+        Ticket ticketRet = null;
+        for (Ticket ticket : ticketlist) {
+            if (ticket.getTicketNum() == ticketNum) {
+                ticketRet = ticket;
+            }
+        }
+        if (ticketRet == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return ticketRet;
+    }
+
+    @PostMapping("/createTicket/{ticketNum}/{title}")
+    public void createTicket(@PathVariable int ticketNum, @PathVariable String title) {
+        Ticket ticket = new Ticket(ticketNum, title);
+        ticketlist.add(ticket);
+    }
+
+    @PostMapping("/updateTicket/{ticketNum}/{title}")
+    public void updateTicket(@PathVariable int ticketNum, @PathVariable String title) {
+        if ((ticketlist == null) || ticketlist.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        for (Ticket ticket : ticketlist) {
+            if (ticket.getTicketNum() == ticketNum) {
+                ticket.setTitle(title);
+            }
         }
     }
 
-    @PostMapping("/{ticketNum}/{title}")
-    public void createTicket(@PathVariable int ticketNum, @PathVariable String title) {
-        ticket = new Ticket(ticketNum, title);
+    @PostMapping("/deleteTicket/{ticketNum}")
+    public void deleteTicket(@PathVariable int ticketNum) {
+        if ((ticketlist == null) || ticketlist.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        for (int i = 0; i < ticketlist.size(); i++) {
+            Ticket ticket = ticketlist.get(i);
+            if (ticket.getTicketNum() == ticketNum) {
+                ticketlist.remove(i);
+            }
+        }
+    }
+
+    @GetMapping("/getAllTickets")
+    public List<Ticket> getAllTickets() {
+        if ((ticketlist == null) || ticketlist.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return ticketlist;
+    }
+
+    @PostMapping("/clearTicketList")
+    public void clearTicketList() {
+        ticketlist.clear();
+    }
+
+    @GetMapping("/getTicketListNum")
+    public int getTicketListNum() {
+        return ticketlist.size();
     }
 }
