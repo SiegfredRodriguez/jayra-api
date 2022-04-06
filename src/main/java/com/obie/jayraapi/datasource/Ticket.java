6a526151ba2d01853ff1eead4aecf976ce57d802
Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2022-04-06 22:30:02 +0800

    002: Added JPA custom equals and hashcode

--- src/main/java/com/obie/jayraapi/datasource/Ticket.java
+++ src/main/java/com/obie/jayraapi/datasource/Ticket.java
@@ -3,11 +3,13 @@ package com.obie.jayraapi.datasource;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -23,5 +25,16 @@ public class Ticket {
     @Column
     private String title;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Ticket ticket = (Ticket) o;
+        return id != null && Objects.equals(id, ticket.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
