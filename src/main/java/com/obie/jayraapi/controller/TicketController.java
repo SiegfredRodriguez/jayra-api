6d52e64a112e1ec404236afe0c5766f7caea4e87
Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2022-04-10 21:05:58 +0800

    003: initial service refactor

--- src/main/java/com/obie/jayraapi/controller/TicketController.java
+++ src/main/java/com/obie/jayraapi/controller/TicketController.java
@@ -1,14 +1,13 @@
 package com.obie.jayraapi.controller;
 
 import com.obie.jayraapi.datasource.Ticket;
-import com.obie.jayraapi.datasource.TicketRepository;
+import com.obie.jayraapi.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,22 +15,16 @@ import java.util.UUID;
 @RequestMapping("/tickets")
 public class TicketController {
 
-    private List<Ticket> ticketlist = new ArrayList<>();
-    private TicketRepository ticketRepository;
+    private TicketService ticketService;
 
     @Autowired
-    public TicketController(TicketRepository ticketRepository) {
-        this.ticketRepository = ticketRepository;
-    }
-
-    @GetMapping("/dummy")
-    public String dummyKo() {
-        return ticketRepository.findById(UUID.fromString("f3c5c58c-bd25-4a8e-be06-30258f16822d")).get().getTitle();
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
     }
 
     @GetMapping("/{ticketNum}")
     public ResponseEntity<Ticket> getTicket(@PathVariable UUID ticketNum) {
-        Optional<com.obie.jayraapi.datasource.Ticket> data = ticketRepository.findById(ticketNum);
+        Optional<com.obie.jayraapi.datasource.Ticket> data = ticketService.findTicket(ticketNum);
         if (data.isPresent()) {
             return new ResponseEntity<>(data.get(), HttpStatus.OK);
         } else {
@@ -42,13 +35,12 @@ public class TicketController {
 
     @PostMapping("/createTicket/{title}")
     public ResponseEntity<Ticket> createTicket(@PathVariable String title) {
-        UUID uuid = UUID.randomUUID();
         Ticket ticket = new Ticket();
-        ticket.setId(uuid);
         ticket.setTitle(title);
+
         try {
-            Ticket ticketRet = ticketRepository.save(ticket);
-            return new ResponseEntity<>(ticket, HttpStatus.CREATED);
+            Ticket ticketRet = ticketService.createTicket(ticket);
+            return new ResponseEntity<>(ticketRet, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -56,22 +48,23 @@ public class TicketController {
 
     @PutMapping("/{ticketNum}/{title}")
     public ResponseEntity<Ticket> updateTicket(@PathVariable UUID ticketNum, @PathVariable String title) {
-        Optional<Ticket> data = ticketRepository.findById(ticketNum);
-        if (data.isPresent()) {
-            Ticket ticket = data.get();
-            ticket.setTitle(title);
-            return new ResponseEntity<>(ticketRepository.save(ticket), HttpStatus.OK);
-        } else {
+
+        Ticket ticket = new Ticket(ticketNum, title);
+        ticket = ticketService.updateTicket(ticket);
+
+        if (Objects.isNull(ticket)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
+        return new ResponseEntity<>(ticket, HttpStatus.OK);
     }
 
     @DeleteMapping("/{ticketNum}")
     public ResponseEntity<Ticket> deleteTicket(@PathVariable(value = "ticketNum") UUID ticketNum) {
-        try {
-            ticketRepository.deleteById(ticketNum);
+        if (ticketService.deleteTicket(ticketNum)) {
+
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
+        } else {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
---
+++ src/main/java/com/obie/jayraapi/service/TicketService.java
@@ -0,0 +1,52 @@
+package com.obie.jayraapi.service;
+
+import com.obie.jayraapi.datasource.Ticket;
+import com.obie.jayraapi.datasource.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TicketService {
+
+    private final TicketRepository ticketRepository;
+
+    @Autowired
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    public Optional<Ticket> findTicket(UUID id) {
+        return ticketRepository.findById(id);
+    }
+
+    public Ticket createTicket(Ticket ticket) {
+        ticket.setId(UUID.randomUUID());
+        return ticketRepository.save(ticket);
+    }
+
+    public Ticket updateTicket(Ticket ticket) {
+        var result = findTicket(ticket.getId());
+
+        if (result.isPresent()) {
+            var t = result.get();
+            t.setTitle(ticket.getTitle());
+            return ticketRepository.save(t);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteTicket(UUID id) {
+        try {
+            ticketRepository.deleteById(id);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
